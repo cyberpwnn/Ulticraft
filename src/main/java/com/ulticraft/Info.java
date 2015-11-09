71e0c97b51147b00a04c4ada1dde822a50c9d801
danielmills <danielmillst@gmail.com>
2015-11-09 02:41:42 -0500

    Achievement Component API continuation

--- src/main/java/com/ulticraft/Info.java
+++ src/main/java/com/ulticraft/Info.java
@@ -21,17 +21,25 @@ public class Info
 	public static final String STAT_MAGICFLAMES = "Magic Flames";
 	public static final String STAT_MAGICRUSHES = "Magic Rushes";
 	
+	public static final int ACHIEVEMENT_MAX = 1000000000;
+	public static final int[] ACHIEVEMENT_PATH = new int[]{10, 100, 200, 300, 400, 500, 1000, 2000, 3000, 4000, 5000, 10000, 20000, 30000, 40000, 50000, 100000, 200000, 300000, 400000, 500000, 1000000, 2000000, 3000000, 4000000, 5000000, 10000000, 20000000, 30000000, 40000000, 50000000, 100000000, 200000000, 300000000, 400000000, 500000000, 1000000000};
+	public static final String[] ACHIEVEMENT_PATH_TITLES = new String[]{"Ten", "One Hundred", "2 Hundred", "3 Hundred", "4 Hundred", "5 Hundred", "One Thousand", "2 Thousand", "3 Thousand", "4 Thousand", "5 Thousand", "Ten Thousand", "20 Thousand", "30 Thousand", "40 Thousand", "50 Thousand", "One Hundred Thousand", "200 Thousand", "300 Thousand", "400 Thousand", "500 Thousand", "One Million", "2 Million", "3 Million", "4 Million", "5 Million", "Ten Million", "20 Million", "30 Million", "40 Million", "50 Million", "One Hundred Million", "200 Million0", "300 Million", "400 Million", "500 Million", "One Billion"};
+	
 	public static final char DINGBAT_PENTAGON = '\u2B1F';
 	public static final char DINGBAT_HEXAGON = '\u2B22';
 	
 	public static final String MSG_GEMS_SPENT = ChatColor.DARK_GRAY + "Spent " + ChatColor.LIGHT_PURPLE + "%s Gems";
 	public static final String MSG_GEMS_EARNED = ChatColor.DARK_GRAY + "Earned " + ChatColor.LIGHT_PURPLE + "%s Gems";
 	public static final String MSG_PERK_UNLOCK = ChatColor.DARK_GRAY + "Unlocked " + ChatColor.LIGHT_PURPLE + "%s";
+	public static final String MSG_ACHIEVEMENT = ChatColor.DARK_GRAY + "Achieved " + ChatColor.LIGHT_PURPLE + "%s %s";
 	
+	public static final String BROAD_ACHIEVEMENT = ChatColor.LIGHT_PURPLE + "%s " + ChatColor.DARK_GRAY + "Achieved " + ChatColor.LIGHT_PURPLE + "%s %s";
 	public static final String BROAD_PERK_UNLOCK = ChatColor.LIGHT_PURPLE + "%s " + ChatColor.DARK_GRAY + "Unlocked " + ChatColor.LIGHT_PURPLE + "%s";
 	
 	public static final USound SOUND_PERK_UNLOCK = new USound("ulticraft.event.unlock");
 	public static final USound SOUND_PERK_UNLOCK_DISTANT = new USound("ulticraft.distantevent.unlock");
+	public static final USound SOUND_ACHIEVE = new USound("ulticraft.event.achieve");
+	public static final USound SOUND_ACHIEVE_DISTANT = new USound("ulticraft.distantevent.achieve");
 	
 	public static final int MANA_BAR_SPLIT = 20;
 	
--- src/main/java/com/ulticraft/component/AchievementComponent.java
+++ src/main/java/com/ulticraft/component/AchievementComponent.java
@@ -1,14 +1,19 @@
 package com.ulticraft.component;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import com.ulticraft.Info;
 import com.ulticraft.Ulticraft;
+import com.ulticraft.composite.Notification;
+import com.ulticraft.composite.PlayerData;
 import com.ulticraft.uapi.Component;
 import com.ulticraft.uapi.Depend;
 
-@Depend(GemComponent.class)
+@Depend({GemComponent.class, NotificationComponent.class})
 public class AchievementComponent extends Component implements Listener
 {
 	public AchievementComponent(Ulticraft pl)
@@ -19,6 +24,7 @@ public class AchievementComponent extends Component implements Listener
 	
 	public void enable()
 	{
+		
 		super.enable();
 	}
 	
@@ -27,9 +33,56 @@ public class AchievementComponent extends Component implements Listener
 		super.disable();
 	}
 	
-	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+	public int getLevel(int at)
+	{
+		int lvl = 0;
+		
+		for(int i : Info.ACHIEVEMENT_PATH)
+		{
+			if(at < i)
+			{
+				return lvl;
+			}
+			
+			lvl++;
+		}
+		
+		return lvl;
+	}
+	
+	public int getReward(int lvl)
+	{
+		return (int) (2 + (Math.pow(lvl + 2, 2) - Math.pow(lvl + 2, 1.6)));
+	}
+	
+	public void check(Player p, String stat, int at)
+	{
+		if(at + 1 > Info.ACHIEVEMENT_MAX)
+		{
+			return;
+		}
+		
+		if(getLevel(at) != getLevel(at + 1))
+		{
+			pl.getNotificationComponent().dispatch(p, new Notification().setSubTitle(String.format(Info.MSG_ACHIEVEMENT, p.getName(), Info.ACHIEVEMENT_PATH_TITLES[getLevel(at + 1)], stat)).setSound(Info.SOUND_ACHIEVE));
+			pl.getGemComponent().give(p, getReward(getLevel(at + 1)));
+			pl.getNotificationComponent().broadcast(p, new Notification().setSubTitle(String.format(Info.BROAD_ACHIEVEMENT, p.getName(), Info.ACHIEVEMENT_PATH_TITLES[getLevel(at + 1)], stat)).setSound(Info.SOUND_ACHIEVE_DISTANT));
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayer(BlockBreakEvent e)
 	{
-		pl.getManaComponent().set(e.getPlayer(), 0f);
+		PlayerData pd = pl.gpd(e.getPlayer());
+		check(e.getPlayer(), Info.STAT_BLOCKBREAK, pd.getBlockBreak());
+		pd.setBlockBreak(pd.getBlockBreak() + 1);
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void onPlayer(BlockPlaceEvent e)
+	{
+		PlayerData pd = pl.gpd(e.getPlayer());
+		check(e.getPlayer(), Info.STAT_BLOCKPLACE, pd.getBlockPlace());
+		pd.setBlockPlace(pd.getBlockPlace() + 1);
 	}
 }
