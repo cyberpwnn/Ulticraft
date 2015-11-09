818fc717086100d79a71017224ecd922ea6e4bfe
danielmills <danielmillst@gmail.com>
2015-11-08 22:38:05 -0500

    Create Gem API

--- src/main/java/com/ulticraft/component/GemComponent.java
+++ src/main/java/com/ulticraft/component/GemComponent.java
@@ -1,5 +1,6 @@
 package com.ulticraft.component;
 
+import org.bukkit.entity.Player;
 import com.ulticraft.Ulticraft;
 import com.ulticraft.uapi.Component;
 import com.ulticraft.uapi.Depend;
@@ -20,5 +21,40 @@ public class GemComponent extends Component
 	public void disable()
 	{
 		super.disable();
+	}
+	
+	public int get(Player p)
+	{
+		return pl.gpd(p).getGems();
+	}
+	
+	public void set(Player p, int g)
+	{
+		pl.gpd(p).setGems(g);
+	}
+	
+	public boolean has(Player p, int g)
+	{
+		return get(p) - g >= 0;
+	}
+	
+	public void give(Player p, int g)
+	{
+		if(g < 1)
+		{
+			return;
+		}
+		
+		set(p, get(p) + g);
+	}
+	
+	public void take(Player p, int g)
+	{
+		if(g < 1 || !has(p, g))
+		{
+			return;
+		}
+		
+		set(p, get(p) - g);
 	}
 }
