f8ef4ddfb7fab66a710fa10c0d4fd9a0a8fb08dd
danielmills <danielmillst@gmail.com>
2015-11-08 23:06:40 -0500

    Touch up Info and Start perks development

--- src/main/java/com/ulticraft/Info.java
+++ src/main/java/com/ulticraft/Info.java
@@ -1,7 +1,13 @@
 package com.ulticraft;
 
+import com.ulticraft.composite.Perk;
+
 public class Info
 {
+	public static final String NAME = "Ulticraft";
+	public static final String VERSION = "3.1.21";
+	public static final String[] AUTHORS = new String[]{"Daniel Mills (cyberpwn)", "Alex Walters (Sir_Ulticraft)"};
+	
 	public static final String STAT_BLOCKBREAK = "Blocks Broken";
 	public static final String STAT_BLOCKPLACE = "Blocks Placed";
 	public static final String STAT_COMBATKILLS = "Combat Kills";
@@ -17,4 +23,8 @@ public class Info
 	public static final char DINGBAT_HEXAGON = '\u2B22';
 	
 	public static final int MANA_BAR_SPLIT = 20;
+	
+	public static final Perk PERK_FLY = new Perk("Fly", new String[]{"Fly anywhere with /fly!"}, new String[]{"essentials.fly", "essentials.fly.safelogin"}, 400);
+
+	public static final Perk[] PERKS = new Perk[]{PERK_FLY};
 }
