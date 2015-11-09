110a554afb21fe33807e59c9b1990246313bede5
danielmills <danielmillst@gmail.com>
2015-11-09 03:13:12 -0500

    Allow checking for if a player can break a block

--- src/main/java/com/ulticraft/component/PermissionComponent.java
+++ src/main/java/com/ulticraft/component/PermissionComponent.java
@@ -1,7 +1,13 @@
 package com.ulticraft.component;
 
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import com.ulticraft.Ulticraft;
 import com.ulticraft.uapi.Component;
@@ -9,14 +15,19 @@ import com.ulticraft.uapi.Depend;
 import com.ulticraft.uapi.UList;
 import net.milkbowl.vault.permission.Permission;
 
+@SuppressWarnings("deprecation")
 @Depend(DataComponent.class)
 public class PermissionComponent extends Component implements Listener
 {
 	private Permission permission;
+	private boolean testEvent;
+	private boolean testResult;
 	
 	public PermissionComponent(Ulticraft pl)
 	{
 		super(pl);
+		testEvent = false;
+		testResult = false;
 	}
 	
 	public void enable()
@@ -27,7 +38,9 @@ public class PermissionComponent extends Component implements Listener
 		{
 			permission = permissionProvider.getProvider();
 		}
-				
+		
+		pl.register(this);
+		
 		super.enable();
 	}
 	
@@ -61,7 +74,7 @@ public class PermissionComponent extends Component implements Listener
 			give(p, i);
 		}
 	}
-
+	
 	public void give(Player p, UList<String> nodes)
 	{
 		for(String i : nodes)
@@ -76,5 +89,30 @@ public class PermissionComponent extends Component implements Listener
 		{
 			give(p, i, w);
 		}
+	}
+	
+	public boolean canBreak(Player p, Block block)
+	{
+		testEvent = true;
+		pl.getServer().getPluginManager().callEvent(new BlockBreakEvent(block, p));
+		
+		return testResult;
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onTest(BlockBreakEvent e)
+	{
+		if(testEvent)
+		{
+			testResult = !e.isCancelled();
+			testEvent = false;
+			e.setCancelled(true);
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onTest(PlayerChatEvent e)
+	{
+		e.setMessage("Value is " + canBreak(e.getPlayer(), e.getPlayer().getLocation().getBlock().getRelative(BlockFace.DOWN)));
 	}
 }
