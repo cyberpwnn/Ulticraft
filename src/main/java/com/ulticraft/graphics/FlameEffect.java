55b784fbc24f6e4b00dbdaa2b81af73b0e9a8d6b
danielmills <danielmillst@gmail.com>
2015-11-09 10:09:54 -0500

    Player Damage!

--- src/main/java/com/ulticraft/graphics/FlameEffect.java
+++ src/main/java/com/ulticraft/graphics/FlameEffect.java
@@ -3,8 +3,8 @@ package com.ulticraft.graphics;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 import com.ulticraft.Ulticraft;
 import com.ulticraft.uapi.Area;
@@ -12,9 +12,12 @@ import com.ulticraft.uapi.ParticleEffect;
 
 public class FlameEffect extends TrailEffect
 {
-	public FlameEffect(Ulticraft pl, Location start, Vector direction)
+	private Player caster;
+	
+	public FlameEffect(Ulticraft pl, Location start, Vector direction, Player caster)
 	{
 		super(pl, start, direction);
+		this.caster = caster;
 		setEffect(ParticleEffect.FLAME);
 		setFilterSet(new VectorFilterSet().add(new RandomVectorFilter(0.1f, 0.1f, 0.1f)));
 	}
@@ -34,15 +37,31 @@ public class FlameEffect extends TrailEffect
 		
 		for(Entity i : a.getNearbyEntities())
 		{
-			if(!i.getType().equals(EntityType.PLAYER))
+			if(i instanceof LivingEntity)
 			{
-				if(i instanceof LivingEntity)
+				if(i instanceof Player)
 				{
-					stop();
-					pi.getLocation().getWorld().createExplosion(pi.getLocation(), 0f);
-					i.setFireTicks(80);
-					((LivingEntity)i).damage(5f);
+					Player t = (Player) i;
+					if(t != caster)
+					{
+						stop();
+						pi.getLocation().getWorld().createExplosion(pi.getLocation(), 0f);
+						i.setFireTicks(80);
+						t.damage(4f);
+						return;
+					}
 				}
+				
+				stop();
+				pi.getLocation().getWorld().createExplosion(pi.getLocation(), 0f);
+				i.setFireTicks(80);
+				((LivingEntity)i).damage(5f);
+				return;
+			}
+			
+			else
+			{
+				
 			}
 		}
 	}
