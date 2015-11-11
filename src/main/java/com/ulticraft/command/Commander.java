bcdf4907d9db08834336838ce619d16c2fb73ef9
danielmills <danielmillst@gmail.com>
2015-11-11 08:33:28 -0500

    Commander Fixes and Improvements

--- src/main/java/com/ulticraft/command/Commander.java
+++ src/main/java/com/ulticraft/command/Commander.java
@@ -3,14 +3,19 @@ package com.ulticraft.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import com.ulticraft.Info;
+import com.ulticraft.Ulticraft;
 import com.ulticraft.uapi.UList;
 
 public class Commander implements CommandExecutor
 {
 	private UList<Node> nodes;
+	private Ulticraft pl;
 	
-	public Commander()
+	public Commander(Ulticraft pl)
 	{
+		this.pl = pl;
 		this.nodes = new UList<Node>();
 	}
 	
@@ -20,9 +25,18 @@ public class Commander implements CommandExecutor
 		
 		for(Node n : nodes)
 		{
+			if(argx.size() == 1)
+			{
+				if(n.getParams().size() == 0 && n.getPath().get(0).equals(argx.get(0)))
+				{
+					possibleNode = n;
+					break;
+				}
+			}
+			
 			for(int i = 0; i < argx.size(); i++)
 			{
-				if(n.getParams().hasIndex(i) && argx.get(i).equalsIgnoreCase(n.getParams().get(i).getName()))
+				if(n.getParams().hasIndex(i) && argx.hasIndex(i + 1) && argx.get(i + 1).equalsIgnoreCase(n.getParams().get(i).getName()))
 				{
 					possibleNode = n;
 				}
@@ -31,10 +45,61 @@ public class Commander implements CommandExecutor
 		
 		if(possibleNode != null)
 		{
+			if(sender instanceof Player)
+			{
+				if(possibleNode.getFlags().contains(NodeFlag.CONSOLE_ONLY))
+				{
+					sender.sendMessage(Info.MSG_CONSOLE_ONLY);
+					return true;
+				}
+			}
 			
+			else
+			{
+				if(possibleNode.getFlags().contains(NodeFlag.PLAYER_ONLY))
+				{
+					sender.sendMessage(Info.MSG_PLAYER_ONLY);
+					return true;
+				}
+			}
+			
+			UList<String> params = argx.copy();
+			params.trimBeginning(possibleNode.getPath().size());
+			
+			if(params.size() == possibleNode.getParams().size())
+			{
+				UList<NodeParamData> npd = new UList<NodeParamData>();
+				
+				int k = 0;
+				
+				for(NodeParam i : possibleNode.getParams())
+				{
+					npd.add(i.toParamData(params.get(k)));
+					k++;
+				}
+				
+				possibleNode.getRunnableNode().run(sender, npd);
+			}
+			
+			else
+			{
+				sender.sendMessage(String.format(Info.MSG_INVALID_PARAMS, params.toString(", "), possibleNode.getParams().toString(", ")));
+				return true;
+			}
+		}
+		
+		else
+		{
+			sender.sendMessage(String.format(Info.MSG_UNKNOWN_SUB_COMMAND, "/" + argx.toString(" ")));
 		}
 		
 		return false;
+	}
+	
+	public Commander addNode(Node n)
+	{
+		nodes.add(n);
+		return this;
 	}
 
 	public UList<Node> getNodes()
