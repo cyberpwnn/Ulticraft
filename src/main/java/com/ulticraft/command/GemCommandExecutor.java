fabe52dddc6bfc4851e2d8b68fbc2c0932c6143d
danielmills <danielmillst@gmail.com>
2015-11-12 00:52:21 -0500

    Finish GemCommandExecutor

--- src/main/java/com/ulticraft/command/GemCommandExecutor.java
+++ src/main/java/com/ulticraft/command/GemCommandExecutor.java
@@ -35,6 +35,131 @@ public class GemCommandExecutor implements CommandExecutor
 			}
 		}
 		
+		else
+		{
+			if(sender.hasPermission(Info.PERM_GEM_GOD))
+			{
+				if(args[0].equalsIgnoreCase("get"))
+				{
+					if(args.length == 2)
+					{
+						if(pl.canFindPlayer(args[1]))
+						{
+							pl.msg(sender, String.format(Info.MSG_PLAYER_GEM_HAS, pl.findPlayer(args[1]).getName(), pl.gpd(pl.findPlayer(args[1])).getGems().toString()));
+						}
+						
+						else
+						{
+							pl.msg(sender, String.format(Info.MSG_CANT_FIND_PLAYER, args[1]));
+						}
+					}
+					
+					else
+					{
+						pl.msg(sender, Info.CMD_HELP_ADMIN_GEM);
+					}
+				}
+				
+				else if(args[0].equalsIgnoreCase("give"))
+				{
+					if(args.length == 3)
+					{
+						if(pl.canFindPlayer(args[1]))
+						{
+							try
+							{
+								int gems = Integer.parseInt(args[2]);
+								
+								if(gems < 0)
+								{
+									pl.msg(sender, String.format(Info.MSG_NOT_POSITIVE, String.valueOf(gems)));
+								}
+								
+								else if(gems < 1)
+								{
+									pl.msg(sender, String.format(Info.MSG_NOT_ZERO, String.valueOf(gems)));
+								}
+								
+								else
+								{
+									pl.getGemComponent().give(pl.findPlayer(args[1]), gems);
+									pl.msg(sender, String.format(Info.MSG_GEMS_GAVE, String.valueOf(gems), pl.findPlayer(args[1]).getName()));
+								}
+							}
+							
+							catch(NumberFormatException e)
+							{
+								pl.msg(sender, String.format(Info.MSG_NOT_NUMBER, args[2]));
+							}
+						}
+						
+						else
+						{
+							pl.msg(sender, String.format(Info.MSG_CANT_FIND_PLAYER, args[1]));
+						}
+					}
+				}
+				
+				else if(args[0].equalsIgnoreCase("take"))
+				{
+					if(args.length == 3)
+					{
+						if(pl.canFindPlayer(args[1]))
+						{
+							try
+							{
+								int gems = Integer.parseInt(args[2]);
+								
+								if(gems < 0)
+								{
+									pl.msg(sender, String.format(Info.MSG_NOT_POSITIVE, String.valueOf(gems)));
+								}
+								
+								else if(gems < 1)
+								{
+									pl.msg(sender, String.format(Info.MSG_NOT_ZERO, String.valueOf(gems)));
+								}
+								
+								else
+								{
+									if(pl.getGemComponent().has(pl.findPlayer(args[1]), gems))
+									{
+										pl.getGemComponent().take(pl.findPlayer(args[1]), gems);
+										pl.msg(sender, String.format(Info.MSG_GEMS_TOOK, String.valueOf(gems), pl.findPlayer(args[1]).getName()));
+									}
+									
+									else
+									{
+										pl.msg(sender, String.format(Info.MSG_GEMS_NOT_ENOUGH, pl.findPlayer(args[1]).getName(), String.valueOf(pl.getGemComponent().get(pl.findPlayer(args[1])))));
+									}
+								}
+							}
+							
+							catch(NumberFormatException e)
+							{
+								pl.msg(sender, String.format(Info.MSG_NOT_NUMBER, args[2]));
+							}
+						}
+						
+						else
+						{
+							pl.msg(sender, String.format(Info.MSG_CANT_FIND_PLAYER, args[1]));
+						}
+					}
+				}
+				
+				else
+				{
+					pl.msg(sender, Info.CMD_HELP_ADMIN_GEM);
+				}
+			}
+			
+			else
+			{
+				pl.msg(sender, Info.CMD_HELP_GEM);
+			}
+		}
+		
 		return true;
 	}
 }
